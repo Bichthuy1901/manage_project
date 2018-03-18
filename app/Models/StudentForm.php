36c9174dfdf5d12ddcace0576dcff44ee5e8e21a
bichthuy1910 <bichthuyth1910@gmail.com>
2018-03-18 15:45:12 +0700

    create project

---
+++ app/Models/StudentForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StudentForm extends Model
+{
+    protected $fillable = [
+         'student_id',
+         'media_id',
+    ];
+}
